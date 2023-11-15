a8a40f3a30ec13899c27398a4b85a9f9e4b08bb3
x10an14-nav <christian.chavez@nav.no>
2023-11-15 16:41:18 +0100

    feature: nixify nada's mkdocs

---
+++ flake.nix
@@ -0,0 +1,105 @@
+{
+  description = "Python for MkDocs";
+
+  # Flake inputs
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  # Flake outputs
+  outputs = {self, ...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {
+        localSystem = {inherit system;};
+      };
+
+      poetryBashScript = {
+        name,
+        poetryCommands,
+      }: pkgs.writeShellApplication {
+        inherit name;
+        runtimeInputs = with pkgs; [
+          git # Don't assume users have a good/recent enough version of git
+          python3
+          poetry
+        ];
+        text = ''
+          repoRoot=$(git rev-parse --show-toplevel)
+          cd "''${repoRoot}"
+          ${poetryCommands}
+        '';
+      };
+
+      setupPoetryName = "setup-mkdocs-poetry";
+      setUpPoetry = poetryBashScript {
+        name = setupPoetryName;
+        poetryCommands = ''
+          poetry install --sync --no-interaction --ansi
+        '';
+      };
+
+      localServeHtmlDocsName = "serve-mkdocs-livereload";
+      localServeHtmlDocs = poetryBashScript {
+        name = localServeHtmlDocsName;
+        poetryCommands = ''
+          poetry run mkdocs serve --clean --watch docs/
+        '';
+      };
+
+      buildHtmlDocsName = "build-mkdocs-html";
+      buildHtmlDocs = poetryBashScript {
+        name = buildHtmlDocsName;
+        poetryCommands = ''
+          poetry run mkdocs build
+        '';
+      };
+
+      htmlDocs = pkgs.stdenv.mkDerivation {
+        src = ./.;
+        name = "nada-docs-html";
+        buildInputs = [setUpPoetry buildHtmlDocs];
+        buildPhase = ''
+          # TODO: Figure out why the below two lines are not executing their respective scripts
+          ''${setupPoetryName}
+          ''${buildHtmlDocsName}
+          mkdir -p $out
+          mv site $out  # Then this `site` folder should exist and be non-empty
+        '';
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.python3
+          pkgs.poetry
+
+          setUpPoetry
+          buildHtmlDocs
+          localServeHtmlDocs
+        ];
+      };
+
+      packages = rec {
+        html = htmlDocs;
+        default = html;
+        ${setupPoetryName} = setUpPoetry;
+        ${localServeHtmlDocsName} = localServeHtmlDocs;
+        ${buildHtmlDocsName} = buildHtmlDocs;
+      };
+
+      apps = {
+        serveHtml = {
+          type = "app";
+          program = "${localServeHtmlDocs}/bin/${localServeHtmlDocsName}";
+        };
+        installMkdocs = {
+          type = "app";
+          program = "${setUpPoetry}/bin/${setupPoetryName}";
+        };
+        buildHtml = {
+          type = "app";
+          program = "${buildHtmlDocs}/bin/${buildHtmlDocsName}";
+        };
+      };
+
+      formatter = pkgs.alejandra;
+    });
+}
